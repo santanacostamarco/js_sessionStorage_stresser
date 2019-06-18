4763d978ddb1a42c760d5ddaed8c73c2d8797678
Marco Santana <santannacostamarco@gmail.com>
2019-06-18 18:10:10 -0300

    session storage stresser

---
+++ SStrgStress.js
@@ -0,0 +1,34 @@
+class SStrgStress {
+	constructor () {
+
+		this.stress(100)
+	}
+
+	generateString(length) {
+	   let 		str     = '';
+	   const 	chars 	= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%¨&*()_-+=\"\'{}[]`´^~?/:;\\,.<>§ªº|€®ŧ←↓→øþĸŋđðßæ«»©“”µ¹²³£¢¬';
+
+	   for ( var i = 0; i < length; i++ ) {
+	      str += chars.charAt(Math.floor(Math.random() * chars.length));
+	   }
+	   return str;
+	}
+
+	stress (length) {
+		let stop = false;
+		let interval = setInterval(() => {
+			try {
+				sessionStorage.setItem(this.generateString(1000), this.generateString(1000));
+				console.log(sessionStorage.length);
+			}
+			catch (e) {
+				clearInterval(interval);
+				if (length < 2)
+					return console.error(e), console.log("length: " + JSON.stringify(sessionStorage).length + " characteres");
+
+				if (e.code === 22)
+					return this.stress(Math.round(length / 2));
+			}
+		}, 100);
+	}
+}
